619ae30ff8af29e0caa0d9fb156db89e1eec21f4
QY Lin <qiu@145.118.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-11-12 14:47:05 +0100

    changed player and main scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,3 +14,26 @@ func _physics_process(delta: float) -> void:
 	velocity = direction * speed
 	move_and_slide()
 	
+	play_animation(direction)
+	
+func play_animation(direction):
+	if player_state == "idle":
+		$AnimatedSprite2D.play("idle")
+	if player_state == "walking":
+		if direction.y == -1:
+			$AnimatedSprite2D.play("n-walk")
+		elif direction.x == 1:
+			$AnimatedSprite2D.play("e-walk")
+		elif direction.y == 1:
+			$AnimatedSprite2D.play("s-walk")
+		elif direction.x == -1:
+			$AnimatedSprite2D.play("w-walk")
+		# diagonal walk
+		elif direction.x > 0.5 and direction.y < -0.5:
+			$AnimatedSprite2D.play("ne-walk")
+		elif direction.x > 0.5 and direction.y > 0.5:
+			$AnimatedSprite2D.play("se-walk")
+		elif direction.x > -0.5 and direction.y < -0.5:
+			$AnimatedSprite2D.play("nw-walk")
+		elif direction.x < -0.5 and direction.y > 0.5:
+			$AnimatedSprite2D.play("sw-walk")
