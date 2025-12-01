297ac4ef62901f5f767fb74b765c3eb783f12a49
QY Lin <qiu@145.118.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-12-01 13:45:07 +0100

    player, tree, inventory changes

--- scripts/apple_tree.gd
+++ scripts/apple_tree.gd
@@ -5,6 +5,9 @@ var player_in_area = false
 
 var apple = preload("res://scenes/apple_collectable.tscn")
 
+@export var item: InventoryItem
+var player = null
+
 func _ready():
 	if state == "no apples":
 		$growth_timer.start()
@@ -18,13 +21,12 @@ func _process(delta):
 			if Input.is_action_just_pressed("e"):
 				state = "no apples"
 				drop_apple()
-		
-		
 
 
 func _on_pickable_area_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		player_in_area = true 
+		player = body
 
 
 func _on_pickable_area_body_exited(body: Node2D) -> void:
@@ -40,6 +42,6 @@ func drop_apple():
 	var apple_instance = apple.instantiate()
 	apple_instance.global_position = $Marker2D.global_position
 	get_parent().add_child(apple_instance)
-	
+	player.collect(item)
 	await get_tree().create_timer(3).timeout
 	$growth_timer.start()
